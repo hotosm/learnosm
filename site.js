957ef9ddb9e21695c34ce85e009fdb2eb60f330a
Jue Yang <jueyang22@gmail.com>
2012-12-17 17:47:02 -0500

    make h1 and h2 linkable in doc layout

--- site.js
+++ site.js
@@ -1,12 +1,17 @@
 $(function() {
-    var app = window.app;
-    $('.language-switcher a').each(function(i, e) {
-        if (app.permalink.length < 3) return;
-        $(e).attr('href',
-            app.baseurl + '/' +
-            $(e).attr('lang') +
-            app.permalink.substr(3)
-        );
-    });
-    $('.language-switcher a[lang=' + app.lang + ']').addClass('active');
+    var learnOsm = {};
+
+    learnOsm.translation = function(){
+        var app = window.app;
+        console.log(app);
+        $('.language-switcher a').each(function(i, e) {
+            if (app.permalink.length < 3) return;
+            $(e).attr('href',
+                app.baseurl + '/' +
+                $(e).attr('lang') +
+                app.permalink.substr(3)
+            );
+        });
+        $('.language-switcher a[lang=' + app.lang + ']').addClass('active');
+    };
 });
