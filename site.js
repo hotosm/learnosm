6ab49b27b1a9619dfee4600c0f20e4ed54ebe12a
Jue Yang <jueyang22@gmail.com>
2012-12-18 15:40:57 -0500

    more styling

--- site.js
+++ site.js
@@ -1,16 +1,21 @@
 $(function() {
-    var learnOsm = {};
+    var app = window.app;
+    $('.language-switcher a').each(function(i, e) {
+        if (app.permalink.length < 3) return;
+        $(e).attr('href',
+            app.baseurl + '/' +
+            $(e).attr('lang') +
+            app.permalink.substr(3)
+        );
+    });
+    $('.language-switcher a[lang=' + app.lang + ']').addClass('active');
 
-    learnOsm.translation = function(){
-        var app = window.app;
-        $('.language-switcher a').each(function(i, e) {
-            if (app.permalink.length < 3) return;
-            $(e).attr('href',
-                app.baseurl + '/' +
-                $(e).attr('lang') +
-                app.permalink.substr(3)
-            );
-        });
-        $('.language-switcher a[lang=' + app.lang + ']').addClass('active');
-    };
+    $(".contact").find("li a").hover(
+        function(){
+          $(this).parent().find("span").addClass("active");
+        },
+        function(){
+          $(this).parent().find("span").removeClass("active");
+        }
+      );
 });
